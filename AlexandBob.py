baa9ca6b584b766f3aef9837f3db4f9d0891d911
Dave <DrReeves@gmail.com>
2016-01-11 15:51:39 -0500

    refactored turtles

--- AlexandBob.py
+++ AlexandBob.py
@@ -1,17 +1,29 @@
 import turtle
 
+inputDegrees = input('How many degrees should Alex turn (0-360)?')
+
+degrees = int(inputDegrees)
+
+
+def turtleSquare(thisTurtle):
+    
+    for c in ['red', 'green', 'yellow', 'blue', 'purple']:
+        thisTurtle.pencolor(c)
+        thisTurtle.forward(75)
+        thisTurtle.left(degrees)
+
+
+def turtleStar(thisTurtle):
+    for i in range(18):
+        Bob.forward(75)
+        Bob.left(190)
+
+
 # create Alex the Turtle
 Alex = turtle.Turtle()
 Alex.shape('turtle')
 
-# make Alex do a colorful square
-for c in ['red', 'green', 'yellow', 'blue', 'purple', 'turquoise']:
-    Alex.pencolor(c)
-    Alex.forward(75)
-    Alex.left(90)
-    
-
-
+  
 # create Bob the turtle
 Bob = turtle.Turtle()
 Bob.shape('turtle')
@@ -20,7 +32,9 @@ Bob.setposition(100, 40)
 Bob.pencolor('blue')
 Bob.pendown()
 
-# make Bob draw a star, and keep doing it forever
-while True:
-    Bob.forward(75)
-    Bob.left(190)
+
+turtleSquare(Alex)
+turtleStar(Bob)
+
+
+
