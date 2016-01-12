1ea6fdc1a82f962569438f2e7d113825a0fdf3de
Xendi <DrReeves@gmail.com>
2016-01-11 23:47:17 -0500

    more general refactor

--- AlexandBob.py
+++ AlexandBob.py
@@ -1,13 +1,10 @@
 import turtle
 
 
-# ask the user for their choice of turtle turn 
-inputDegrees = raw_input('How many degrees should Alex turn (0-360)?')
-# convert the choice (a string) into a number (an int)
-degrees = int(inputDegrees)
+
 
 # these are the two functions that describe what we want the turtles to do. They take one argument, which is the turtle of choice.
-def turtleSquare(thisTurtle):
+def turtlePolygon(thisTurtle):
     
     for c in ['red', 'green', 'yellow', 'blue', 'purple']:
         thisTurtle.pencolor(c)
@@ -31,9 +28,14 @@ Bob.setposition(100, 40)
 Bob.pencolor('blue')
 Bob.pendown()
 
+# ask the user for their choice of turtle turn 
+inputDegrees = raw_input('How many degrees should a turtle turn when making a polygon? (0-360)?')
+# convert the choice (a string) into a number (an int)
+degrees = int(inputDegrees)
+
 userWait = raw_input('We did not make the turtles do anything yet. Time to call the functions and pass the turtles! Press Ok or Enter to continue')
 
-turtleSquare(Alex)
+turtlePolygon(Alex)
 turtleStar(Bob)
 turtleStar(Alex)        # now Alex doesn't feel left out. He's a star too...
 
